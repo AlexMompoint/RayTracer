088b4b460490806e78d373d1043ba93c1711ae75
Alexander Mompoint <alexmompoint@gmail.com>
2024-09-24 01:30:39 -0400

    Inital Commit of a window

---
+++ RayTracer/RayTracer/main.cpp
@@ -0,0 +1,55 @@
+
+#include <GLFW/glfw3.h>
+#include <chrono>
+#include <thread>
+#include <stdio.h>
+
+using namespace std;
+using namespace std::chrono_literals;
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main()
+{
+    
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        //std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glViewport(0, 0, 800, 600);
+
+
+    auto currTime = chrono::system_clock::now();
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+
+        this_thread::sleep_for(20ms - (chrono::system_clock::now() - currTime));
+        currTime = chrono::system_clock::now();
+    }
+
+
+    glfwTerminate();
+    return 0;
+}
